5eec5c67472f71e896e9fe5364d3235a5efab0dc
Joseph Sivits <33550671+josephsivits@users.noreply.github.com>
2022-02-21 14:59:54 -0600

    refactored images and trying to get it to work lol

--- app/src/main/java/com/example/stockbuddyversion1/MainActivity.java
+++ app/src/main/java/com/example/stockbuddyversion1/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.stockbuddyversion1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        // admin and admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    // correct
+                    Toast.makeText(MainActivity.this, "Login Successful!",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Unsuccessful :(",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
     }
 }
